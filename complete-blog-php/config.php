1987d64de41bed150c511ea2387f0530e92d3a8b
sadhan <sadhan@creditmantri.com>
2019-07-12 15:08:21 +0530

    login page

---
+++ complete-blog-php/config.php
@@ -0,0 +1,7 @@
+<?php 
+	session_start();
+	// connect to database
+    // define global constants
+	define ('ROOT_PATH', realpath(dirname(__FILE__)));
+	define('BASE_URL', 'http://localhost/complete-blog-php/');
+?>
